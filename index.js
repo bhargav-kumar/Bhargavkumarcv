5a4fc7e9103bbce4dfee1c5d189f1f6420246b47
bhargav-kumar <ch.bhargav_kumar@hotmail.com>
2023-03-05 11:16:52 +0530

    Added hamburger menu

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let myButton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -14,4 +14,12 @@ function scrollFunction() {
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+function navWrapper() {
+  document.querySelector('.navbar-toggler'),
+    navToogler = document.querySelector('.navigationToggle')
+  navToogler.addEventListener('click', function (event) {
+    navWrapper.classList.toggle('active')
+  });
 }
